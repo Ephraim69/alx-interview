810bf3547a870a806e23019d76f7caac3fcf0ef2
Ephraim Igbinosa <106774289+Ephraim69@users.noreply.github.com>
2023-07-14 05:48:34 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n < 2:
+        return 0
+    res = 0
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            res += i
+    if n > 1:
+        res += n
+    return res
