48169f69b520bb452ed6db7186f7c9824553de9b
Ephraim Igbinosa <106774289+Ephraim69@users.noreply.github.com>
2023-07-14 05:52:00 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file
+"""
+
 
 def minOperations(n):
     if n < 2:
@@ -14,4 +19,3 @@ def minOperations(n):
     if n > 1:
         res += n
     return res
-
