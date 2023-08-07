1991fe3651680851aa44158dd99fdec4a0ee0406
Ephraim Igbinosa <106774289+Ephraim69@users.noreply.github.com>
2023-08-07 01:05:52 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,66 +1,53 @@
 #!/usr/bin/python3
+
+"""
+Solution for N-Queens problem
+"""
+
 import sys
 
-def is_safe(board, row, col, n):
-    # Check the row on the left side
-    for i in range(col):
-        if board[row][i] == 1:
+def valid_position(chessboard, x, y, size):
+    """
+    Check if a queen can be placed at (x, y) without being attacked.
+    """
+    for i in range(x):
+        if (chessboard[i] == y or 
+            chessboard[i] - i == y - x or 
+            chessboard[i] + i == y + x):
             return False
-
-    # Check upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on the left side
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
-def solve_nqueens_util(board, col, n, solutions):
-    if col >= n:
-        solutions.append([row[:] for row in board])  # deep copy of the board
+def solve(chessboard, x, size):
+    """
+    Recursively attempt to place a queen in a valid spot.
+    """
+    if x == size:
+        print([[i, chessboard[i]] for i in range(size)])
         return
 
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
-            solve_nqueens_util(board, col + 1, n, solutions)
-            board[i][col] = 0  # backtrack
+    for y in range(size):
+        if valid_position(chessboard, x, y, size):
+            chessboard[x] = y
+            solve(chessboard, x + 1, size)
 
-def solve_nqueens(n):
-    board = [[0] * n for _ in range(n)]
-    solutions = []
-    solve_nqueens_util(board, 0, n, solutions)
-    return solutions
-
-def format_solutions(solutions):
-    formatted_solutions = []
-    for solution in solutions:
-        current_solution = []
-        for i in range(len(solution)):
-            for j in range(len(solution[i])):
-                if solution[i][j] == 1:
-                    current_solution.append([i, j])
-        formatted_solutions.append(current_solution)
-    return formatted_solutions
+def main(size):
+    """
+    Main function to kick off the solution.
+    """
+    if size < 4:
+        print("Size should be 4 or more.")
+        sys.exit(1)
+    
+    chessboard = [-1 for _ in range(size)]
+    solve(chessboard, 0, size)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Correct usage: script_name <board size>")
         sys.exit(1)
-    
-    N = sys.argv[1]
-    if not N.isdigit():
-        print("N must be a number")
+    try:
+        board_size = int(sys.argv[1])
+    except ValueError:
+        print("Please provide a valid integer for the board size.")
         sys.exit(1)
-    N = int(N)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    solutions = solve_nqueens(N)
-    formatted_solutions = format_solutions(solutions)
-    for solution in formatted_solutions:
-        print(solution)
+    main(board_size)
