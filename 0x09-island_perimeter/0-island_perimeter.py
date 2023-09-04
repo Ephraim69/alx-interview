f0798589afe42e9bc2b5906605df6b19f3455e52
Ephraim Igbinosa <106774289+Ephraim69@users.noreply.github.com>
2023-09-04 05:23:09 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,1 +1,30 @@
-8
+#!/usr/bin/python3
+"""
+0-island_perimeter module
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    if not grid or not grid[0]:
+        return 0
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Check top
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
